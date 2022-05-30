b429e601bfeb0683998077ac301ed9f95e1c677f
MetaCubeX <maze.y2b@github.com>
2022-05-31 00:04:25 +0800

    chore: change latencyTestUrl

--- src/api/proxies.ts
+++ src/api/proxies.ts
@@ -33,7 +33,7 @@ export async function requestToSwitchProxy(apiConfig, name1, name2) {
 export async function requestDelayForProxy(
   apiConfig,
   name,
-  latencyTestUrl = 'http://www.gstatic.com/generate_204'
+  latencyTestUrl = 'http://www.gstatic.com/generate_202'
 ) {
   const { url, init } = getURLAndInit(apiConfig);
   const qs = `timeout=5000&url=${latencyTestUrl}`;
--- src/store/app.ts
+++ src/store/app.ts
@@ -159,7 +159,7 @@ const defaultState: StateApp = {
   selectedClashAPIConfigIndex: 0,
   clashAPIConfigs: [defaultClashAPIConfig],
 
-  latencyTestUrl: 'http://www.gstatic.com/generate_204',
+  latencyTestUrl: 'http://www.gstatic.com/generate_202',
   selectedChartStyleIndex: 0,
   theme: 'dark',
 
